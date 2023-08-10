55711d0f64e4e153f5e311ecb16456533efaba72
Philip Riecks <mail@philipriecks.de>
2023-08-10 15:44:33 +0200

    clean up for coding along

--- src/main/java/de/rieckpil/CommentApiController.java
+++ src/main/java/de/rieckpil/CommentApiController.java
@@ -1,7 +1,5 @@
 package de.rieckpil;
 
-import org.springframework.web.bind.annotation.*;
-
 /**
  * Develop a REST API to retrieve and create comments. Everybody should be able to retrieve comments
  * but only logged-in users with the role ADMIN can create a comment.
