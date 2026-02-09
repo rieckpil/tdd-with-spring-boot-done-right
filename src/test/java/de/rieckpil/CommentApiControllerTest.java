c291659d047afc3714cf5f2d8119cf412cc3a48b
Philip Riecks <mail@philipriecks.de>
2026-02-09 14:04:25 +0100

    upgrade to Spring Boot 4 (#23)

--- src/test/java/de/rieckpil/CommentApiControllerTest.java
+++ src/test/java/de/rieckpil/CommentApiControllerTest.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
