4e41e9c9315954549e2efc170eda1588628bea42
Philip Riecks <mail@philipriecks.de>
2026-03-18 10:44:57 +0100

    prepare new video

--- src/main/resources/db/migration/V001__create-book-table.sql
+++ src/main/resources/db/migration/V001__create-book-table.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS books (
     isbn VARCHAR(20) NOT NULL UNIQUE,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
-    published_date DATE NOT NULL,
-    status VARCHAR(20) NOT NULL
+    published_date DATE NOT NULL
 );
